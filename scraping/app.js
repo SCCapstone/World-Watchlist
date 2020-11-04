1664462db04f9d70d0904f266273a7fac0a35c7b
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2020-11-04 17:15:42 -0500

    .gitignore and npm req added.

--- scraping/app.js
+++ scraping/app.js
@@ -3,6 +3,17 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const request = require('request')
+request('https://www.reddit.com/r/programming.json', function (
+  error,
+  response,
+  body
+) {
+  console.error('error:', error)
+  console.log('body:', body)
+})
+
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
