075fb21f82dda2f5e3f72b6c6f5b801b22895a46
zhymirt <dzhymir@gmail.com>
2020-11-07 14:34:33 -0500

    finished lines to parse links from JSON Object and write to screen

--- scraping/app.js
+++ scraping/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 var convert = require('xml-js');
 const hostname = '127.0.0.1';
 const port = 3000;
-
+let links = []
 // const request = require('request')
 // request('https://newsapi.org/v2/everything?q=bitcoin&apiKey=608f2a765753454fa4b75aea9f5c53f5', function (
 //   error,
@@ -22,14 +22,17 @@ request('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', function (
 ) {
   var result1 = convert.xml2json(body, {compact: true, spaces: 4});
   var info = JSON.parse(result1)
-  console.log(info.rss.channel)
-  
+  for ( i = 0 ; i < info.rss.channel.item.length ; ++i )
+    links.push(info.rss.channel.item[i].link._text);
+  console.log(links);
 })
 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  for(let i = 0 ; i < links.length ; ++i)
+    res.write(links[i]+'\n');
   res.end('Hello World');
 });
 
