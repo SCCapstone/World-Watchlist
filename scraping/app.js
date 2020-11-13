b179df0d70f7b0c2d6a4e4b8a94b34c4730373f9
zhymirt <dzhymir@gmail.com>
2020-11-13 18:12:47 -0500

    transferred from requests to axios

--- scraping/app.js
+++ scraping/app.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const axios = require('axios');
 var convert = require('xml-js');
 const hostname = '127.0.0.1';
 const port = 3000;
 let links = []
+let links2 = []
+
+function removeEntries(list) {
+  for( i = 0 ; i < list.length ; ++i ) {
+    if list[i].includes("puzzle") ||
+      list[i].includes("crossword") {
+        list.pop
+      }
+  }
+}
 // const request = require('request')
 // request('https://newsapi.org/v2/everything?q=bitcoin&apiKey=608f2a765753454fa4b75aea9f5c53f5', function (
 //   error,
@@ -13,7 +24,19 @@ let links = []
 //   var info = JSON.parse(body)
 //   console.log('body:', info.articles)
 // })
-
+axios.get('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml').then(
+  (response) => {
+    // console.log(response);
+    // console.log(response.data);
+    let result2 = convert.xml2json(response.data, {compact: true, spaces: 4});
+    // console.log(result2);
+    let info2 = JSON.parse(result2)
+    for ( i = 0 ; i < info2.rss.channel.item.length ; ++i )
+      links2.push(info2.rss.channel.item[i].link._text);
+    console.log(links2);
+  }).catch((error) => {
+    console.log(error);
+  })
 const request = require('request')
 request('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', function (
   error,
