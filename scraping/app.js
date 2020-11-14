61b4c6fc719bb1c01d0f31f50b2d0ba07a328231
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2020-11-14 15:11:15 -0500

    express package added

--- scraping/app.js
+++ scraping/app.js
@@ -1,9 +1,8 @@
-const http = require('http');
 const axios = require('axios');
 var convert = require('xml-js');
-const hostname = '127.0.0.1';
+const express = require('express')
+const app = express()
 const port = 3000;
-let links = [];
 let links2 = [];
 let article_info = [];
 
@@ -16,17 +15,9 @@ function removeEntries(list) {
       }
   }
 }
-// const request = require('request')
-// request('https://newsapi.org/v2/everything?q=bitcoin&apiKey=608f2a765753454fa4b75aea9f5c53f5', function (
-//   error,
-//   response,
-//   body
-// ) {
-//   console.error('error:', error)
-//   var info = JSON.parse(body)
-//   console.log('body:', info.articles)
-// })
-axios.get('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml').then(
+
+app.get('/', (req, res) => {
+  axios.get('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml').then(
   (response) => {
     // console.log(response);
     // console.log(response.data);
@@ -47,33 +38,15 @@ axios.get('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml').then(
       article_info.push(temp);
       links2.push(info2.rss.channel.item[i].link._text);
     }
-    console.log(article_info);
+    // console.log(article_info);
+    res.send(article_info);
     // console.log(links2);
   }).catch((error) => {
     console.log(error);
   })
-/*const request = require('request')
-request('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml', function (
-  error,
-  response,
-  body
-) {
-  var result1 = convert.xml2json(body, {compact: true, spaces: 4});
-  var info = JSON.parse(result1)
-  for ( i = 0 ; i < info.rss.channel.item.length ; ++i )
-    links.push(info.rss.channel.item[i].link._text);
-  console.log(links);
-})*/
+  
+})
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  for(let i = 0 ; i < links.length ; ++i)
-    res.write(links[i]+'\n');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port) 
+console.log("listetning on http://localhost:" + port);
