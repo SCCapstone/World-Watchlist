4db4114811bf4632ea11e62277108298191021a9
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2020-11-21 15:26:34 -0500

    change to new firebase server

--- project-folder/src/API/store.js
+++ project-folder/src/API/store.js
@@ -1,17 +1,17 @@
 const admin = require('firebase-admin');
 const axios = require('axios')
-const serviceAccount = require('./RSSFEED_DB-f800edfd5b6b.json');
+const serviceAccount = require('./\world-watchlist-server-fa5a3-firebase-adminsdk-1gbjp-b433d05376.json');
 /*
 For now, put bbc news info into BBCNews Collection
 using the end of url as doc id
 and {title, desc, link} as fields.
 */
-function  bbcpost() {
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+function bbcpost() {
     axios.post("http://localhost:5000/bbc").then(({ data }) => {
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
-        });
-        const db = admin.firestore();
         var URL_ID, urlLink, title, description
         var articles = data
         console.log(articles.length)
@@ -30,8 +30,10 @@ function  bbcpost() {
         }
     });
 }
-
-bbcpost();
+function thisInterval() {
+    bbcpost();
+}
+setInterval(thisInterval, 6000);
 
 
 
