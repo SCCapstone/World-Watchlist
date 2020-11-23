24b09c230abe5a943150b59c475ed7e9c62867fb
zhymirt <dzhymir@gmail.com>
2020-11-23 16:12:42 -0500

    modified store.js to be called with function, included nyt

--- project-folder/src/API/store.js
+++ project-folder/src/API/store.js
@@ -10,28 +10,79 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 const db = admin.firestore();
+function writeDoc(articles, doc_name) {
+    let URL_ID, urlLink, title, description;
+    // let articles = data;
+    console.log(articles.length);
+    let article_list = [];
+    for (i = 0; i < articles.length; i++) {
+        title = articles[i].title;
+        description = articles[i].description;
+        urlLink = articles[i].link;
+        article_list.push({'Title': title, 'Description': description, 'Link': urlLink});
+        URL_ID = (urlLink.split('/').pop());
+        // db.collection('BBCNews').doc(URL_ID).set(
+        //     {
+        //         Title: title,
+        //         Description: description,
+        //         Link: urlLink
+        //     }
+        // );
+    }
+    db.collection('articles').doc(doc_name).set(
+        {articles: article_list}
+    )
+}
 function bbcpost() {
     axios.post("http://localhost:5000/bbc").then(({ data }) => {
-        var URL_ID, urlLink, title, description
-        var articles = data
-        console.log(articles.length)
-        for (i = 0; i < articles.length; i++) {
-            title = articles[i].title
-            description = articles[i].description
-            urlLink = articles[i].link
-            URL_ID = (urlLink.split('/').pop())
-            db.collection('BBCNews').doc(URL_ID).set(
-                {
-                    Title: title,
-                    Description: description,
-                    Link: urlLink
-                }
-            );
-        }
+        writeDoc(data, 'BBCNews');
+        // var URL_ID, urlLink, title, description
+        // var articles = data
+        // console.log(articles.length)
+        // let article_list = []
+        // for (i = 0; i < articles.length; i++) {
+        //     title = articles[i].title
+        //     description = articles[i].description
+        //     urlLink = articles[i].link
+        //     article_list.push({'Title': title, 'Description': description, 'Link': urlLink})
+        //     URL_ID = (urlLink.split('/').pop())
+        //     // db.collection('BBCNews').doc(URL_ID).set(
+        //     //     {
+        //     //         Title: title,
+        //     //         Description: description,
+        //     //         Link: urlLink
+        //     //     }
+        //     // );
+        // }
+        // db.collection('articles').doc('BBCNews').set(
+        //     {articles: article_list}
+        // )
+    });
+}
+function nytpost() {
+    axios.post("http://localhost:5000/nyt").then(({ data }) => {
+        writeDoc(data, 'NYTNews')
+        // var URL_ID, urlLink, title, description
+        // var articles = data
+        // console.log(articles.length)
+        // for (i = 0; i < articles.length; i++) {
+        //     title = articles[i].title
+        //     description = articles[i].description
+        //     urlLink = articles[i].link
+        //     URL_ID = (urlLink.split('/').pop())
+        //     db.collection('NYTNews').doc(URL_ID).set(
+        //         {
+        //             Title: title,
+        //             Description: description,
+        //             Link: urlLink
+        //         }
+        //     );
+        // }
     });
 }
 function thisInterval() {
     bbcpost();
+    nytpost();
 }
 setInterval(thisInterval, 6000);
 
--- server/functions/index.js
+++ server/functions/index.js
@@ -32,6 +32,33 @@ app.post('/bbc', (req, res) => {
     }
     // console.log(article_info)
     res.send(article_info);
+    article_info = [];
+    // console.log(links2)
+  }).catch((error) => {
+    console.log(error);
+  })
+})
+app.post('/nyt', (req, res) => {
+  url = "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+  axios.get(url).then(
+  (response) => {
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600') // loads information quicker by caching the results to user.
+    let result2 = convert.xml2json(response.data, {compact: true, spaces: 4});
+    let info2 = JSON.parse(result2)
+    for ( i = 0 ; i < info2.rss.channel.item.length ; ++i ) {
+      item = info2.rss.channel.item[i];
+      console.log(info2.rss.channel.item[i]);
+      let title = link = description = image = null;
+      title = f.getTitle(item);
+      link = f.getLink(item);
+      description = f.getDesc(item);
+      temp = new f.article(title, description, link);
+      article_info.push(temp);
+      links2.push(info2.rss.channel.item[i].link._text);
+    }
+    // console.log(article_info)
+    res.send(article_info);
+    article_info = [];
     // console.log(links2)
   }).catch((error) => {
     console.log(error);
