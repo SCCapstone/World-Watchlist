47ffeec820e0909da951812feba60f0f9546decf
zhymirt <dzhymir@gmail.com>
2020-12-02 13:31:45 -0500

    Added back Article.tsx and ArticleList.tsx

--- project-folder/src/components/ArticleList.tsx
+++ project-folder/src/components/ArticleList.tsx
@@ -3,8 +3,8 @@ import Article from './Article';
 import { article, articleList } from './articleTypes';
 
 function ArticleList(props: {theArticleList: articleList}) {
-    //console.log(props.theArticleList);
-    let articles = props.theArticleList.map((x: article) =><div key={x.title}> <Article theArticle={x}></Article> </div>);
+    console.log(props.theArticleList);
+    let articles = props.theArticleList.map((x: article) => <Article theArticle={x}></Article>);
 return <div>{articles}</div>;
 }
 export default ArticleList;
