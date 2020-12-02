f4f82b2258e0844b55ecc543a77e19ce58a6fc9c
zhymirt <dzhymir@gmail.com>
2020-12-02 18:13:45 -0500

    Added css elements and added link name to articles

--- project-folder/src/components/Article.tsx
+++ project-folder/src/components/Article.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { article } from './ArticleTypes';
-
+import './Article.css'
 
 function Article(props: {theArticle: article}) {
     //console.log(props.theArticle);
-    return <div>
-        <h3>{props.theArticle.title}</h3>
-        <span><a href={props.theArticle.link}></a></span>
-        <p>{props.theArticle.description}</p>
+    return <div id="Container">
+        <h3 id="title">{props.theArticle.title}</h3>
+        <span><a href={props.theArticle.link}>{props.theArticle.link}</a></span>
+        <p id="description">{props.theArticle.description}</p>
     </div>;
 }
 export default Article;
