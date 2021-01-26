e7d70ee88c8c639f414872b7de14133e93fdcfc3
zhymirt <dzhymir@gmail.com>
2021-01-26 17:12:30 -0500

    Started implementation for blocking users

--- project-folder/src/pages/Social.tsx
+++ project-folder/src/pages/Social.tsx
@@ -24,6 +24,7 @@ type MyState = {
   ourUsername: string;
   targetUsername: string;
   friendsList: string[];
+  blockedFriends: string[];
   isPendingRequestsModalOpen: boolean;
   isMessengerModalOpen: boolean;
   incomingRequests: string[];
@@ -43,6 +44,7 @@ class Social extends React.Component<MyProps, MyState> {
     ourUsername: '',
     targetUsername: '',
     friendsList: [],
+    blockedFriends: [],
     isPendingRequestsModalOpen: false,
     isMessengerModalOpen: false,
     incomingRequests: [],
@@ -50,6 +52,7 @@ class Social extends React.Component<MyProps, MyState> {
     messageRef: ''
   };
   unsubscribeFriendsList: any;
+  unsubscribeBlockedFriends: any;
   unsubscribeIncomingRequests: any;
   unsubscribeOutgoingRequests: any;
 
@@ -64,6 +67,7 @@ class Social extends React.Component<MyProps, MyState> {
     this.declineFriend = this.declineFriend.bind(this);
     this.cancelOutgingRequest = this.cancelOutgingRequest.bind(this);
     this.toggleMessengerModal = this.toggleMessengerModal.bind(this);
+    this.blockFriend = this.blockFriend.bind(this);
     //End Function Bindings
 
     //Begin firebase data subscriptins
@@ -76,6 +80,12 @@ class Social extends React.Component<MyProps, MyState> {
           this.unsubscribeFriendsList = db.collection('friends').doc(this.state.ourUsername).onSnapshot((snapshot) => {
             if(snapshot.data()) {
               this.setState({friendsList: snapshot.data()!.friendsList})
+            }
+          })
+
+          this.unsubscribeBlockedFriends = db.collection('blockedFriends').doc(this.state.ourUsername).onSnapshot((snapshot) => {
+            if(snapshot.data()) {
+              this.setState({blockedFriends: snapshot.data()!.blockedFriends})
             }
           })
 
@@ -102,6 +112,7 @@ class Social extends React.Component<MyProps, MyState> {
   componentWillUnmount() {
     //since we have subscriptions, we cancel them here to prevent a memory leak
     this.unsubscribeFriendsList()
+    this.unsubscribeBlockedFriends()
     this.unsubscribeIncomingRequests()
     this.unsubscribeOutgoingRequests()
   }
@@ -176,6 +187,14 @@ class Social extends React.Component<MyProps, MyState> {
       db.collection('incomingFriendRequests').doc(this.state.ourUsername).update({
         incomingFriendRequests: firebase.firestore.FieldValue.arrayRemove(username),
       })
+    }
+  }
+
+  blockFriend(username: string) { // adds given friend to blocked list, users on blocked list are excluded from stuff
+    if(username != "") {
+      let chosenFriend = db.collection('friends').doc(username);
+      db.collection('blockedFriends').add(chosenFriend);
+      // db.collection('friends').doc(username).delete() // remove user once added to blocked list, line included for future possibility
     }
   }
 
