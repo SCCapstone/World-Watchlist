192aa5a9de0f65e761742d59afcd50a6467bb812
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2021-02-15 15:30:58 -0500

    Subscribing will create collection if topic doesn't exist.

--- project-folder/src/pages/Feed.tsx
+++ project-folder/src/pages/Feed.tsx
@@ -37,7 +37,8 @@ type MyState = {
   showLoading:boolean;
   showModal:boolean,
   collectionExist:boolean,
-  showSubscription:boolean
+  showSubscription:boolean,
+  allArticles:any[]
 }
 
 type MyProps = {
@@ -56,7 +57,8 @@ class Feed extends React.Component<MyProps, MyState> {
     showLoading:false,
     showModal:false,
     collectionExist:false,
-    showSubscription:false
+    showSubscription:false,
+    allArticles:[]
   };
 
   constructor(props: MyProps) {
@@ -69,16 +71,15 @@ class Feed extends React.Component<MyProps, MyState> {
             console.log('current user: ' + doc.data()!.username)
             this.setState({CurrentUser:doc.data()!.username})
           }
-
           // get subscription list .get().then
           db.collection("topicSubscription").doc(this.state.CurrentUser).onSnapshot((sub_list) => {
             if (sub_list.exists) {
-              console.log(sub_list.data()!.subList)
+              console.log("current sub list: ", sub_list.data()!.subList)
               this.setState({subs: sub_list.data()!.subList});
               let aList : articleList = [];
               if (this.state.subs.length !== 0) {
                 for (var i = 0; i < this.state.subs.length; i++) {
-                let unsubscribeArticles = NewsDB.collection(this.state.subs[i]).get()
+                NewsDB.collection(this.state.subs[i]).get()
                 .then((snapshot) => {
                   snapshot.forEach(doc => {
                     if (doc.exists) {
@@ -93,8 +94,9 @@ class Feed extends React.Component<MyProps, MyState> {
                   console.log("Error getting document:", error);
                 })
               }
-                this.setState({articles: aList})
             }
+            /* reset articles in ui each sub and unsub */
+            this.setState({articles: aList})
             } else {
               db.collection("topicSubscription").doc(this.state.CurrentUser).set({subList: []});
             }
@@ -104,23 +106,29 @@ class Feed extends React.Component<MyProps, MyState> {
             console.log("Error getting document:", error);
         });
       }
-      
+      /* store news in 'all' collection into a state to reduce reading*/
+      let Allnews: any[] = []
+      NewsDB.collection('all').get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          Allnews.push(doc.data())
+      });
+      })
+      this.setState({allArticles:Allnews})
+      console.log("allarticles", this.state.allArticles)
     })
   }
 
   /* can currently subscribe to: gaming, health, politics, sports, technology, world */
-  addSubscription(sub: string) {
-    if ( sub !== "") {
-      db.collection("topicSubscription").doc(this.state.CurrentUser).update({subList: firebase.firestore.FieldValue.arrayUnion(sub)})
+  async addSubscription(sub: string) {
+    if (sub!== "") {
+      await db.collection("topicSubscription").doc(this.state.CurrentUser).update({subList: firebase.firestore.FieldValue.arrayUnion(sub)})
     }
   }
 
   removeSubscription(index:any) {
-    this.state.subs.splice(index,1)
     if (this.state.subs[index] !== "") {
-      db.collection("topicSubscription").doc(this.state.CurrentUser).update({subList: (this.state.subs)})
+      db.collection("topicSubscription").doc(this.state.CurrentUser).update({subList: firebase.firestore.FieldValue.arrayRemove(this.state.subs[index])})
     }
-    console.log(this.state.subs)
   }
 
   ParentComponent = (props:any) => (
@@ -135,8 +143,7 @@ class Feed extends React.Component<MyProps, MyState> {
   <IonCard>
         <IonCardHeader >
           <IonCardTitle >{props.subscription}</IonCardTitle>
-          <IonButton size="small" color="dark" type="submit" expand="full" shape="round" onClick={()=> this.removeSubscription(props.index)}>unsub</IonButton>
-
+          <IonButton size="small" color="dark" type="submit" expand="full" shape="round" onClick={()=> this.unsubscribe(props.subscription,props.index)}>unsub</IonButton>
         </IonCardHeader>
       </IonCard>  
 
@@ -146,46 +153,62 @@ class Feed extends React.Component<MyProps, MyState> {
       console.log("Enter a valid topic");
     } else {
     let aList : articleList = [];
-    let unsubscribeArticles = NewsDB.collection(topic.toLowerCase()).get()
-    .then((snapshot) => {
+    NewsDB.collection(topic).get().then((snapshot) => {
+      /* Searching topics based on string matching titles to input if a collection doesn't exist */
+      if (snapshot.empty) {
+        var filtered = this.state.allArticles.filter(doc => doc.Title.toLowerCase().includes(topic))
+        if (filtered.length === 0) {
+          console.log("currently no news on that topic")
+        }
+        filtered.forEach(filtered => {
+          aList.push({title: filtered.Title, link: filtered.Link, description: filtered.Description})
+        })
+        this.setState({articlesSearched: aList})
+      } else {
       aList = []
       snapshot.forEach(doc => {
         if (doc.exists) {
           this.setState({collectionExist:true})
           let articleItem = doc.data();
           aList.push({title: articleItem.Title, link: articleItem.Link, description: articleItem.Description})
-        } else {
-          console.log("Cannot find anything in database.")
         }
       })
       this.setState({articlesSearched: aList})
+      }
     }).catch(function(error) {
       console.log("Error getting document:", error);
   });
-    this.setState({unsubscribeArticles: unsubscribeArticles})
   }
   }
-  async subscribe(topic:any) {
-    let aCollection = NewsDB.collection(topic.toLowerCase()).get().then((snapshot) => {
-      if ( snapshot.empty) {
-        console.log("Cannot find any news on that topic.")
+
+  subscribe(topic:any) {
+    if (topic === null || topic === undefined || topic === '') {
+      console.log("Enter a valid topic");
+    } else {
+     NewsDB.collection(topic.toLowerCase()).get().then((snapshot) => {
+       /* Creating a new collection if topic collection doesn't exist and subscribing to it */
+      if (snapshot.empty) {
+        var filteredArticles = this.state.allArticles.filter(doc => doc.Title.toLowerCase().includes(topic))
+        if (filteredArticles.length === 0) {
+          console.log("currently no news on that topic")
+        }
+        filteredArticles.forEach(filteredArticles => {
+          NewsDB.collection(topic).doc(filteredArticles.Title).set({Title:filteredArticles.Title, Link: filteredArticles.Link, Description: filteredArticles.Description});
+        })
+        console.log("Making new collection called", topic, " and subscribing")
+        this.addSubscription(topic);
       } else {
         console.log("News about "+ topic +" has been found and will be subscribed.")
         this.addSubscription(topic);
         this.setState({collectionExist:false})        
       }
     });
-    // console.log("Reference"+aCollection);
-    // if( /*this.state.collectionExist*/) {
-    //   console.log("News about "+ topic +" has been found and will be subscribed.")
-    //   this.addSubscription(topic);
-    //   this.setState({collectionExist:false})
-    // } else {
-    //   console.log("Cannot find any news on that topic.")
-    // }
-    
+    }
   }
-
+  unsubscribe(topic:any,index:any) {
+    console.log("News about "+ topic +" has been found and will be unsubscribed.")
+    this.removeSubscription(index);
+  }
   render() {
     const subs = [];
       for (var i = 0; i < this.state.subs.length; i+=1) {
@@ -242,11 +265,11 @@ class Feed extends React.Component<MyProps, MyState> {
       </IonButton>
       <IonLoading
         isOpen={this.state.showLoading}
-        onDidDismiss={() => this.searchTopic(this.state.topicSearched) && this.setState({showLoading: false})}
+        onDidDismiss={() => this.searchTopic(this.state.topicSearched.toLowerCase()) && this.setState({showLoading: false})}
         message={'Getting data from database'}
         duration={150}
       />
-      <IonButton size="default" color="dark" type="submit" expand="full" shape="round" onClick={()=>this.subscribe(this.state.topicSearched)}>
+      <IonButton size="default" color="dark" type="submit" expand="full" shape="round" onClick={()=>this.subscribe(this.state.topicSearched.toLowerCase())}>
           subscribe
       </IonButton>
     <ArticleList theArticleList={this.state.articlesSearched}></ArticleList>
@@ -266,15 +289,12 @@ class Feed extends React.Component<MyProps, MyState> {
         </IonToolbar>
         </IonHeader>
         <IonContent>
-          
         <IonCard>
         <this.ParentComponent>
        {subs}
       </this.ParentComponent>
         </IonCard>
-        
         </IonContent>
-        
     </IonModal>
       <ArticleList theArticleList={this.state.articles}></ArticleList>
       </IonContent>
