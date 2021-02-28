224615c5fdf3407ca4b2cf09676c755f4c9da51e
kristinamatthews <kkm1@email.sc.edu>
2021-02-28 11:49:02 -0500

    accept merge

--- project-folder/src/components/FeedTypes.ts
+++ project-folder/src/components/FeedTypes.ts
@@ -16,33 +16,9 @@ export type FeedState = {
     isSearchingModal:boolean,
     showSearchAlert:boolean,
     showSubscribeAlert:boolean,
-<<<<<<< HEAD
-    isChanging:boolean
-}
-export type GroupFeedState = {
-  articles: articleList;
-  subs: string[];
-  articlesSearched:any[],
-  subscribedArticles: any;
-  CurrentUser:any;
-  topicSearched:any;
-  showLoading:boolean;
-  showModal:boolean,
-  showSubscription:boolean,
-  allArticles:any[],
-  locationBased:boolean,
-  isSearchingModal:boolean,
-  showSearchAlert:boolean,
-  showSubscribeAlert:boolean,
-  isChanging:boolean,
-  isWeatherModalOpen: boolean,
-  groupId: string;
-}
-=======
     isChanging:boolean,
   }
   
->>>>>>> 424bbdd94799a9ad1ee9ab6cea4a33de224ed1a9
 export type FeedProps = {
     history: any;
     location: any;
