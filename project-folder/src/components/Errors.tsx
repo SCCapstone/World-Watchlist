97d25c9791fcd6f8aaa083462228d11f8355448b
zhymir thompson <dzhymir@gmail.com>
2021-03-10 14:23:45 -0500

    continuing error styling

--- project-folder/src/components/Errors.tsx
+++ project-folder/src/components/Errors.tsx
@@ -1,10 +1,11 @@
 import { IonList, IonItem } from "@ionic/react";
 import React from "react";
 import './Errors.css';
+import '../pages/Landing.css'
 
 function Errors(props: {errors: string[]}) {
     let messages = props.errors.map((msg: string,
-         index: number) => {return <IonItem key={index} class="ErrorMessages">{msg}</IonItem>})
+         index: number) => {return <IonItem key={index} color="danger" class="ErrorMessages">{msg}</IonItem>})
     return (<IonList>
         {messages}
     </IonList>);
