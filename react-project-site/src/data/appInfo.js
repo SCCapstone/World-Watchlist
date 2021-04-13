f18760d8fcf17bbf8b90809f7930554fa292bd5e
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2021-04-13 19:28:30 -0400

    putting react-site folder in branch.

---
+++ react-project-site/src/data/appInfo.js
@@ -0,0 +1,7 @@
+const appInfo = {
+    "WhatIs": {header: "World Watchlist", body: "A news watch-list app that will allow users to denote keywords or topics and to receive live notifications when an article is published matching their keyword or topic."},
+    "WhatFor": {header: "", body: ""},
+    "WhereInstructions": {header: "", body: ""}
+}
+
+export default appInfo
