1d010247c6e03263648c0e58ad22fd548697f824
zhymir thompson <dzhymir@gmail.com>
2021-04-13 13:44:11 -0400

    Made data folder

---
+++ react-project-site/src/data/appInfo.js
@@ -0,0 +1,5 @@
+export const appInfo = {
+    "WhatIs": {header: "", body: ""},
+    "WhatFor": {header: "", body: ""},
+    "WhereInstructions": {header: "", body: ""}
+}
---
+++ react-project-site/src/data/teamInfo.js
@@ -0,0 +1,29 @@
+export const team = [
+    {
+        name: "Clay Mallory",
+        linkedin: undefined,
+        pic: undefined
+    },
+    {
+        name: "Justin Taylor",
+        linkedin: undefined,
+        pic: undefined
+    },
+    {
+        name: "Kristina Matthews",
+        linkedin: undefined,
+        pic: undefined
+    },
+    {
+        name: "Nguyen Nguyen",
+        linkedin: undefined,
+        pic: undefined
+    },
+    {
+        name: "Zhymir Thompson",
+        linkedin: undefined,
+        pic: undefined
+    }
+];
+
+export const ourRepo = "https://github.com/SCCapstone/World-Watchlist";
