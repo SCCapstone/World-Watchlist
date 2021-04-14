ae6604ae5602639a4dcb307fa2f900e5fe9dbf6d
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2021-04-14 12:39:45 -0400

    react website branch instead of master

--- react-project-site/src/App.js
+++ react-project-site/src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import './App.css';
 import Home from './pages/Home'
+import { Navbar,Nav} from 'react-bootstrap'
+import React from 'react'
+import logo from './images/icon.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Home/>
-      </header>
+      <Navbar className="nav" fixed="top" >
+        <Nav className="mr-auto">
+          <Nav.Link 
+            onClick={() => scroller.scrollTo('Home', {
+            smooth: true,
+            offset: -70,
+            duration: 500,
+            })}
+          >
+            <img
+              src={logo}
+              width="28"
+              height="28"
+            />
+          </Nav.Link>
+          <Nav.Link id="navlink" >About Us</Nav.Link>
+          <Nav.Link id="navlink" >Demo</Nav.Link>
+          <Nav.Link id="navlink" >Team</Nav.Link>
+          <Nav.Link id="navlink" >Github</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div>
+        <header className="App-header">
+        <Element name="Home"><Home/></Element>
+        </header>   
+      </div>          
     </div>
+
   );
 }
 export default App;
