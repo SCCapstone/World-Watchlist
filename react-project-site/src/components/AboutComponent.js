45a7efc8bb6355473bec8c9f00010971c9632419
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2021-04-15 20:10:51 -0400

    format team in columns.

--- react-project-site/src/components/AboutComponent.js
+++ react-project-site/src/components/AboutComponent.js
@@ -1,11 +1,16 @@
+import {Container,Row,Col} from 'react-bootstrap'
 
 function AboutComponent({teamMembers}) {
     let members = teamMembers.map((member) => {
-        return <div>
+        return <Col>
         <h4>{member.name}</h4>
         {member.linkedin ? <span>LinkedIn: {member.linkedin}</span> : <br></br>}
-        </div>
+        </Col>
     })
-        return <div>{members}</div>;
+        return <Container>
+                    <Row>
+                        {members}
+                    </Row>
+                </Container>
 }
 export default AboutComponent;
