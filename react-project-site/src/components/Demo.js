06a3231849d4de966fab928716297d2ef41747de
Nguyen Vu Nguyen <39196799+nvnguyen-ki@users.noreply.github.com>
2021-04-26 19:17:56 -0400

    add final demo video to website.

--- react-project-site/src/components/Demo.js
+++ react-project-site/src/components/Demo.js
@@ -4,7 +4,7 @@ function Demo() {
     return <div className="demo" style={{ width: 660, margin:'auto' }}>
           <h2 id="title">Demo</h2>
     <ResponsiveEmbed aspectRatio="16by9">
-    <iframe  width="1904" height="800" src="https://www.youtube.com/embed/TYkRaj9VwZU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe src="https://drive.google.com/file/d/1ZY-BqUK7iglykKTF4bZYS6k6t5JEX6-q/preview" width="640" height="480"></iframe>
     </ResponsiveEmbed>
   </div>
 }
